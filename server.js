1da3a0de60d93c725e0bb3b489b8c1f385937a1f
Isa Conteh <iconteh21@gmail.com>
2020-09-19 20:53:38 -0400

    put css in external stylesheet, linked and used it

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const nameRoutes = require('./routes/funny-name');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + "/public"));
 
 app.use(homeRoutes);
 app.use(nameRoutes);
